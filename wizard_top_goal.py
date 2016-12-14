ffa38aba2a94bf61eae4588bf8fa304701965a8b
Dman89 <upnvapesd@gmail.com>
2016-12-13 19:03:05 -0800

    Select and Check Top 3 Priorities

--- wizard_top_goal.py
+++ wizard_top_goal.py
@@ -44,3 +44,43 @@ def add_item():
             return add_item()
     else:
         return res
+
+def selecting_priorities(req):
+    clear()
+    x=1
+    for item in req:
+        print("""
+        #{} - {}
+        """.format(x,item))
+        x+=1
+    return x
+
+def top_3_select(req):
+    p("""
+    Select the Top 3 Goals:
+    """)
+    n1 = top_3_check("First")
+    n2 = unique_check("Second", n1, top_3_check("Second"), None)
+    n3 = unique_check("Third", n1, n2, top_3_check("Third"))
+    clear()
+    print("""
+    {}
+    {}
+    {}
+    """.format(req[n1-1], req[n2-1], req[n3-1]))
+    return [req[n1-1], req[n2-1], req[n3-1]]
+
+def unique_check(text, n1, n2, n3):
+    if n3:
+        if n3 != n1:
+            if n3 != n2:
+                return n3
+            else:
+                return unique_check(text, n1, n2, top_3_check("{} [NOT in use] ".format(text)))
+        else:
+            return unique_check(text, n1, n2, top_3_check("{} [NOT in use] ".format(text)))
+    else:
+        if n2 != n1:
+            return n2
+        else:
+            return unique_check(text, n1, top_3_check("{} [NOT in use] ".format(text)), None)
