cb1229da498e4ad6455458dd655f36c5e19a76cf
Dman89 <upnvapesd@gmail.com>
2016-12-28 19:11:06 -0800

    List Options Functions added

--- wizard_daily_todo.py
+++ wizard_daily_todo.py
@@ -30,3 +30,59 @@ def daily_todo_start(usrnam):
     while (run_mode == 1):
         function_selector()
     return todos
+
+def list_todos():
+    clear()
+    print("""
+    Listing Daily Todos:
+    """)
+    list_todos_function()
+
+def list_todos_function():
+    global todos
+    x=0
+    data = []
+    for items in todos:
+        x+=1
+        print("""
+        #{} - {}
+        """.format(x, items))
+        data.append(items)
+        for item in todos[items]:
+            print("""
+            Name: {}
+            Value: {}""".format(item['name'], item['rank']))
+        print("\n")
+    return data
+
+def function_selector():
+    global todos
+    global username
+    global run_mode
+    print("""
+ADD, EDIT, REMOVE, SAVE, or LIST Daily Todos
+    To Exit: Type EXIT or SE to Save and Exit""")
+    selector = input(str("""
+> """))
+    if "ADD" in selector:
+        add_todos()
+    elif "EDIT" in selector:
+        edit_todos()
+    elif "SAVE" in selector:
+        save_todos(username, todos)
+    elif "REMOVE" in selector:
+        remove_todos()
+    elif "LIST" in selector:
+        list_todos()
+    elif "EXIT" in selector:
+        run_mode = 0
+    elif "SE" in selector:
+        save_todos(username, todos)
+        run_mode = 0
+
+def save_todos(username, todos):
+    with open("{}_daily.json".format(username), mode="w") as file:
+        json.dump({"daily": todos}, file)
+        print("""
+        Saved File...
+        """)
